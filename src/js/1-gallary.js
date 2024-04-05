fa357822aa27cddefe2bb3835f85f53b40c72cf5
Kosdevua <konstantin.vodolozhko@gmail.com>
2024-04-05 09:53:34 +0300

    added dependence

--- src/js/1-gallary.js
+++ src/js/1-gallary.js
@@ -1,1 +1,9 @@
-
+// Описаний в документації
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
+// ініціализація
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionData: 'alt',
+  CaptionDelay: 250,
+});
