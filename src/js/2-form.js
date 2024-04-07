f869ed94c64594552e048e1d74c907b93a8ce8d8
Kosdevua <konstantin.vodolozhko@gmail.com>
2024-04-07 21:08:04 +0300

    sdssc

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,5 +1,57 @@
-function onInput(event) {
-  const { email, message } = event.currentTarget.elements;
-  userData.email = email.value.trim();
-  userData.message = message.value.trimo;
-}
+// Використовуючи делегуваня, відстежуй на формі подію input
+// щоразу записуй у локальне сховище об'єкт з полями email і message, у яких зберігай поточні значення полів форми.
+// Нехай ключем для сховища буде "рядок"
+
+// "feedback-form-state"
+
+// Під час завантаження сторінки перевіряй стан сховища
+// , і якщо там є збережені дані, то заповнюй ними поля форми.
+// В іншому випадку поля повинні бути порожніми.
+// Під час сабміту форми - очищай сховище і поля форми, а також
+// виводь у консоль об'єкт з полями email, message та їхніми поточними значеннями.
+
+// рефи записані в змінні
+
+const feedbackFormEl = document.querySelector('.feedback-form');
+const userData = {};
+
+const fillFeedbackFormFields = () => {
+  try {
+    const userDataFromLs = JSON.parse(localStorage.getItem('userFeedbackInfo'));
+    if (userDataFromLs === null) {
+      return;
+    }
+
+    for (const key in userDataFromLs) {
+      feedbackFormEl.elements[key].value = userDataFromLs[key];
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+fillFeedbackFormFields();
+
+const onFeedbackFieldChange = event => {
+  event.preventDefault();
+  const { target: feedbackFieldEl } = event;
+  // console.log(feedbackFieldEl);
+  const feedbackFieldValue = feedbackFieldEl.value;
+  const feedbackFieldName = feedbackFieldEl.name;
+  // console.log('name: ', feedbackFieldName);
+  // console.log('value: ', feedbackFieldValue);
+
+  userData[feedbackFieldName] = feedbackFieldValue;
+
+  localStorage.setItem('userFeedbackInfo', JSON.stringify(userData));
+};
+
+const onFeedbackFromSubmit = event => {
+  event.preventDefault();
+  console.log(userData);
+  localStorage.removeItem('userFeedbackInfo');
+  feedbackFormEl.reset();
+};
+
+feedbackFormEl.addEventListener('change', onFeedbackFieldChange);
+feedbackFormEl.addEventListener('submit', onFeedbackFromSubmit);
